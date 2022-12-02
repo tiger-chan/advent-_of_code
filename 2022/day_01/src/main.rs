7ff9da799396f139eb6b71711e94c12c10f93adc
Anthony <anthony.r.young@gmail.com>
2022-12-01 17:27:38 -0700

    Update for top 3 calories

--- 2022/day_01/src/main.rs
+++ 2022/day_01/src/main.rs
@@ -2,23 +2,22 @@ use std::fs;
 
 fn main() {
     let elves_data = fs::read_to_string("./data/elf.txt").unwrap();
-	let mut max_elf_id = 0;
-	let mut max_elf_value = 0;
-	let mut elf_id = 0;
-	let mut cur_total = 0;
-	for entry in elves_data.split('\n') {
-		if let Ok(v) = i32::from_str_radix(entry, 10) {
-			cur_total += v;
-		} else {
-			if max_elf_value <= cur_total {
-				max_elf_id = elf_id;
-				max_elf_value = cur_total;
-			}
-			cur_total = 0;
-		}
 
-		elf_id += 1;
-	}
+    let mut elves: [i32; 4] = [0, 0, 0, 0];
+    for entry in elves_data.split('\n') {
+        if let Ok(v) = i32::from_str_radix(entry, 10) {
+            elves[0] += v;
+        } else {
+            for i in 1..elves.len() {
+                if elves[i - 1] > elves[i] {
+                    elves.swap(i - 1, i);
+                }
+            }
+            elves[0] = 0;
+        }
+    }
 
-	print!("elf: {}, value: {}\n", max_elf_id, max_elf_value);
+    let sum: i32 = elves[1..elves.len()].iter().sum();
+
+    print!("calories: {}\n", sum);
 }
