d30c25b9369c66e9f6aef406cfa0a87fe21e6710
tiger-chan <anthony.r.young@gmail.com>
2022-12-08 18:50:33 -0700

    day_06 step 2

--- 2022/day_05/main.rs
+++ 2022/day_05/main.rs
@@ -1,85 +1,85 @@
-use std::{fs, collections::VecDeque};
+use std::{collections::VecDeque, fs};
 
 fn is_between(l: char, u: char, c: char) -> bool {
-	l <= c && c <= u
+    l <= c && c <= u
 }
 
 fn main() {
     let data_str = fs::read_to_string("./data/crates.txt").unwrap();
 
-	let mut iter = data_str.split('\n');
+    let mut iter = data_str.split('\n');
 
-	let create_count = if let Some(line) = iter.clone().next() {
-		(line.len() / 4) + 1
-	} else {
-		panic!("Malformed input file");
-	};
+    let create_count = if let Some(line) = iter.clone().next() {
+        (line.len() / 4) + 1
+    } else {
+        panic!("Malformed input file");
+    };
 
-	let mut crates: Vec<VecDeque<char>> = Vec::new();
-	crates.resize(create_count, VecDeque::new());
+    let mut crates: Vec<VecDeque<char>> = Vec::new();
+    crates.resize(create_count, VecDeque::new());
 
     'crate_list: while let Some(line) = iter.next() {
-		if line.is_empty() {
-			break 'crate_list;
-		}
+        if line.is_empty() {
+            break 'crate_list;
+        }
 
-		for entry in 0..create_count {
-			let value = if let Some(c) = line.chars().nth(entry * 4 + 1) {
-				c
-			} else {
-				panic!("Malformed input file");
-			};
+        for entry in 0..create_count {
+            let value = if let Some(c) = line.chars().nth(entry * 4 + 1) {
+                c
+            } else {
+                panic!("Malformed input file");
+            };
 
-			if is_between('l', '0', value) {
-				break 'crate_list;
-			}
+            if is_between('l', '0', value) {
+                break 'crate_list;
+            }
 
-			if is_between('A', 'Z', value) {
-				crates[entry].push_front(value);
-			}
-		}
+            if is_between('A', 'Z', value) {
+                crates[entry].push_front(value);
+            }
+        }
     }
 
-	let mut tmp_stack: String = String::new();
-	while let Some(line) = iter.next() {
-		let split = line.split(' ');
-		let split_val: Vec<&str> = split.clone().collect();
-		let quantity = if let Some(x) = split_val.iter().nth(1) {
-			usize::from_str_radix(x, 10).unwrap()
-		} else {
-			panic!("Malformed input file");
-		};
-		let src = if let Some(x) = split_val.iter().nth(3) {
-			usize::from_str_radix(x, 10).unwrap() - 1
-		} else {
-			panic!("Malformed input file");
-		};
-		let dest = if let Some(x) = split_val.iter().nth(5) {
-			usize::from_str_radix(x, 10).unwrap() - 1
-		} else {
-			panic!("Malformed input file");
-		};
+    let mut tmp_stack: String = String::new();
+    while let Some(line) = iter.next() {
+        let split = line.split(' ');
+        let split_val: Vec<&str> = split.clone().collect();
+        let quantity = if let Some(x) = split_val.iter().nth(1) {
+            usize::from_str_radix(x, 10).unwrap()
+        } else {
+            panic!("Malformed input file");
+        };
+        let src = if let Some(x) = split_val.iter().nth(3) {
+            usize::from_str_radix(x, 10).unwrap() - 1
+        } else {
+            panic!("Malformed input file");
+        };
+        let dest = if let Some(x) = split_val.iter().nth(5) {
+            usize::from_str_radix(x, 10).unwrap() - 1
+        } else {
+            panic!("Malformed input file");
+        };
 
-		tmp_stack.clear();
-		for _ in 0..quantity {
-			let src = &mut crates[src];
-			if let Some(item) = src.pop_back() {
-				tmp_stack.push(item);
-			}
-		}
+        tmp_stack.clear();
+        for _ in 0..quantity {
+            let src = &mut crates[src];
+            if let Some(item) = src.pop_back() {
+                tmp_stack.push(item);
+            }
+        }
 
-		for i in tmp_stack.chars().rev() {
-			let stack = &mut crates[dest];
-			stack.push_back(i);
-		}
-	}
+        for i in tmp_stack.chars().rev() {
+            let stack = &mut crates[dest];
+            stack.push_back(i);
+        }
+    }
 
-	let mut top: String = String::new();
-	for item in crates {
-		if let Some(c) = item.back() {
-			top.push(c.clone());
-		}
-	}
+    let mut top: String = String::new();
+    for item in crates {
+        if let Some(c) = item.back() {
+            top.push(c.clone());
+        }
+    }
 
     println!("Top of crates stacks: {:?}", top);
 }
--- 2022/day_06/main.rs
+++ 2022/day_06/main.rs
@@ -1,35 +1,42 @@
-use std::{fs};
+use std::fs;
 
-fn all_unique(d: &[char; 4]) -> bool {
-	for x in 0..d.len() {
-		for y in (x + 1)..d.len() {
-			if d[x] == d[y] {
-				return false;
-			}
-		}
-	}
-	return true;
+const SIZE: usize = 14;
+
+fn all_unique(d: &[char; SIZE]) -> bool {
+    for x in 0..d.len() {
+        for y in (x + 1)..d.len() {
+            if d[x] == d[y] {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 fn main() {
     let data_str = fs::read_to_string("./data/start_of_packet.txt").unwrap();
 
-	if data_str.len() < 4 {
-		return
-	}
+    if data_str.len() < SIZE {
+        return;
+    }
 
-	let mut data: [char; 4] = data_str.chars().take(4).collect::<Vec<char>>().try_into().unwrap();
-	let mut i = 3;
-	let mut result = 0;
-	for n in 3..data_str.len() {
-		data[i] = data_str.chars().nth(n).unwrap();
-		i = (i + 1) % 4;
+    let mut data: [char; SIZE] = data_str
+        .chars()
+        .take(SIZE)
+        .collect::<Vec<char>>()
+        .try_into()
+        .unwrap();
+    let mut i = SIZE - 1;
+    let mut result = 0;
+    for n in (SIZE - 1)..data_str.len() {
+        data[i] = data_str.chars().nth(n).unwrap();
+        i = (i + 1) % SIZE;
 
-		if all_unique(&data) {
-			result = n + 1;
-			break;
-		}
-	}
+        if all_unique(&data) {
+            result = n + 1;
+            break;
+        }
+    }
 
     println!("[2022][06] {}", result);
 }
