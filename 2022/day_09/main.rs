aea7bdafed7d2c76810d24ef1bbeec5324fca8b2
tiger-chan <anthony.r.young@gmail.com>
2022-12-11 21:44:36 -0700

    day_09 step 2

--- 2022/day_09/main.rs
+++ 2022/day_09/main.rs
@@ -1,26 +1,90 @@
-use std::fs;
+use std::{
+    fs,
+    ops::{Add, AddAssign, Neg},
+};
 
 mod vector;
 
 use crate::vector::Vector;
 
+const KNOT_COUNT: usize = 10;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    Nil,
+}
+
+impl Neg for Direction {
+    type Output = Direction;
+    fn neg(self) -> Self::Output {
+        match self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+            Direction::Nil => Direction::Nil,
+        }
+    }
+}
+
+impl From<Direction> for Vector {
+    fn from(d: Direction) -> Self {
+        match d {
+            Direction::Up => Vector::up(),
+            Direction::Down => Vector::down(),
+            Direction::Left => Vector::left(),
+            Direction::Right => Vector::right(),
+            Direction::Nil => Vector::new(0, 0),
+        }
+    }
+}
+
+impl From<Vector> for Direction {
+    fn from(d: Vector) -> Self {
+        match d {
+            Vector { x: _, y: 1 } => Direction::Up,
+            Vector { x: _, y: -1 } => Direction::Down,
+            Vector { x: -1, y: _ } => Direction::Left,
+            Vector { x: 1, y: _ } => Direction::Right,
+            _ => Direction::Nil,
+        }
+    }
+}
+
+impl Add<Direction> for Vector {
+    type Output = Vector;
+
+    fn add(self, rhs: Direction) -> Self::Output {
+        self + Vector::from(rhs)
+    }
+}
+
+impl AddAssign<Direction> for Vector {
+    fn add_assign(&mut self, rhs: Direction) {
+        self.add_assign(Vector::from(rhs))
+    }
+}
+
 fn main() {
     let data_str = fs::read_to_string("./data/day_09.txt").unwrap();
 
-    let mut head: Vector = Vector::new(0, 0);
-    let mut tail: Vector = Vector::new(0, 0);
+    let mut knots: [Vector; KNOT_COUNT] = [Vector::new(0, 0); KNOT_COUNT];
     let mut positions: Vec<Vector> = Vec::new();
-    positions.push(tail);
+    positions.push(knots[0]);
 
     for line in data_str.split("\n") {
         if line.len() < 3 {
             panic!("Malformed input file");
         }
-        let offset = match &line[..1] {
-            "U" => Vector::up(),
-            "D" => Vector::down(),
-            "L" => Vector::left(),
-            "R" => Vector::right(),
+        let dir = match &line[..1] {
+            "U" => Direction::Up,
+            "D" => Direction::Down,
+            "L" => Direction::Left,
+            "R" => Direction::Right,
             _ => panic!("Malformed input file"),
         };
 
@@ -31,12 +95,24 @@ fn main() {
         };
 
         for _ in 0..repeat {
-            head += offset;
+            knots[0] += dir;
 
-            if (head - tail).mag() > 1.5 {
-                // too far away so move the tail closer
-                tail = head - offset;
-                positions.push(tail);
+            for k in 1..knots.len() {
+                let delta = knots[k] - knots[k - 1];
+                knots[k] = match delta.abs() {
+                    Vector { x: 2, y: 2 } => knots[k - 1] + Vector::clamp(delta, -1, 1),
+                    Vector { x: 2, y: _ } => {
+                        knots[k - 1] + Vector::clamp(delta * Vector::right(), -1, 1)
+                    }
+                    Vector { x: _, y: 2 } => {
+                        knots[k - 1] + Vector::clamp(delta * Vector::up(), -1, 1)
+                    }
+                    _ => knots[k],
+                };
+            }
+
+            if let Some(&l) = knots.last() {
+                positions.push(l);
             }
         }
     }
--- 2022/day_09/vector.rs
+++ 2022/day_09/vector.rs
@@ -1,4 +1,4 @@
-use std::{ops, cmp::Ordering};
+use std::{cmp::Ordering, ops};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct Vector {
@@ -27,8 +27,24 @@ impl Vector {
         Vector::new(-1, 0)
     }
 
-	pub fn mag(&self) -> f32 {
-		((self.x * self.x + self.y * self.y) as f32).sqrt()
+    pub fn mag_sqr(&self) -> f32 {
+        (self.x * self.x + self.y * self.y) as f32
+    }
+
+    #[allow(dead_code)]
+    pub fn mag(&self) -> f32 {
+        self.mag_sqr().sqrt()
+    }
+
+    pub fn clamp(v: Self, min: i32, max: i32) -> Self {
+        Vector::new(
+            i32::min(i32::max(min, v.x), max),
+            i32::min(i32::max(min, v.y), max),
+        )
+    }
+
+	pub fn abs(&self) -> Self {
+		Vector::new(self.x.abs(), self.y.abs())
 	}
 }
 
@@ -54,33 +70,40 @@ impl ops::Neg for Vector {
 }
 
 impl ops::AddAssign for Vector {
-	fn add_assign(&mut self, rhs: Self) {
-		self.x += rhs.x;
-		self.y += rhs.y;
-	}
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
 }
 
 impl ops::SubAssign for Vector {
-	fn sub_assign(&mut self, rhs: Self) {
-		self.x -= rhs.x;
-		self.y -= rhs.y;
-	}
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+}
+
+impl ops::Mul for Vector {
+    type Output = Vector;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Vector::new(self.x * rhs.x, self.y * rhs.y)
+    }
 }
 
 impl Ord for Vector {
-	fn cmp(&self, rhs: &Self) -> Ordering {
-		match self.x.cmp(&rhs.x) {
-			Ordering::Equal => self.y.cmp(&rhs.y),
-			x => x
-		}
+    fn cmp(&self, rhs: &Self) -> Ordering {
+        match self.x.cmp(&rhs.x) {
+            Ordering::Equal => self.y.cmp(&rhs.y),
+            x => x,
+        }
     }
 }
 
 impl PartialOrd for Vector {
-	fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
-		match self.x.partial_cmp(&rhs.x) {
-			Some(Ordering::Equal) => self.y.partial_cmp(&rhs.y),
-			x => x
-		}
-	}
+    fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
+        match self.x.partial_cmp(&rhs.x) {
+            Some(Ordering::Equal) => self.y.partial_cmp(&rhs.y),
+            x => x,
+        }
+    }
 }
