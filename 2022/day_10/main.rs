ecf22a4f870996df5e288f1e92616faac4451028
tiger-chan <anthony.r.young@gmail.com>
2022-12-12 22:46:22 -0700

    day_10 step 1

--- 2022/day_10/main.rs
+++ 2022/day_10/main.rs
@@ -1,9 +1,44 @@
 use std::fs;
 
+const SIGNALS: usize = 256;
+
 fn main() {
-    let _data_str = fs::read_to_string("./data/day_10.txt").unwrap();
+    let data_str = fs::read_to_string("./data/day_10.txt").unwrap();
 
-	let signal_strength = 0;
+    let mut signals: [i32; SIGNALS] = [0; SIGNALS];
+    signals[0] = 1;
 
-    println!("[2022][10] signal strength: {}", signal_strength);
+    let mut cycle: usize = 0;
+    for line in data_str.split("\n") {
+        match line {
+            "noop" => {
+                cycle = cycle + 1;
+                signals[cycle] += signals[cycle - 1];
+            }
+            _ => {
+                let num_str = &line[5..];
+                if let Ok(num) = num_str.parse::<i32>() {
+                    cycle = cycle + 2;
+                    signals[cycle - 1] += signals[cycle - 2];
+                    signals[cycle] += signals[cycle - 1];
+                    signals[cycle] += num;
+                }
+            }
+        }
+    }
+
+    for _ in 0..2 {
+        cycle = cycle + 1;
+        signals[cycle] += signals[cycle - 1];
+    }
+
+    let mut signal_points: [i32; 6] = [0; 6];
+    for i in 0..signal_points.len() {
+        let idx = 19 + i * 40;
+        signal_points[i] = signals[idx] * (idx + 1) as i32;
+    }
+
+    let signal_str = signal_points.iter().fold(0, |x, y| x + y);
+
+    println!("[2022][10] signal strength: {}", signal_str);
 }
